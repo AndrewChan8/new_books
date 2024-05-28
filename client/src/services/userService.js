ec782881326360f5e624cac1c536a463fdd90919
AndrewSushi <andrewsushi8@gmail.com>
2024-05-27 21:53:28 -0700

    Cleaned up the user services from the front end

--- client/src/services/userService.js
+++ client/src/services/userService.js
@@ -3,11 +3,6 @@ import axios from 'axios';
 
 const API_URL = 'http://localhost:4000/api/users';
 
-export const getUsers = async () => {
-  const response = await axios.get(API_URL);
-  return response.data;
-};
-
 export const createUser = async (user) => {
   const response = await axios.post(API_URL, user);
   return response.data;
