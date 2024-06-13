3fc3df798f363a9ffef3c80289e66b71c3d8ecb3
AndrewSushi <andrewsushi8@gmail.com>
2024-06-12 22:58:14 -0700

    Changed the navbar.js

--- client/src/components/NavBar.js
+++ client/src/components/NavBar.js
@@ -1,62 +1,26 @@
-// src/pages/SignIn.js
-import React, { useState } from 'react';
-import { useNavigate } from 'react-router-dom';
-import { authenticateUser } from '../services/userService';
+import Container from 'react-bootstrap/Container';
+import Nav from 'react-bootstrap/Nav';
+import Navbar from 'react-bootstrap/Navbar';
 
-import EmailIcon from "../img/EmailIcon.png";
-import PasswordIcon from "../img/PasswordIcon.png";
-
-const SignIn = ({toggleForm}) => {
-  const [email, setEmail] = useState('');
-  const [password, setPassword] = useState('');
-  const navigate = useNavigate();
-
-  const handleSubmit = async (e) => {
-    e.preventDefault();
-    const credentials = { email, password };
-    try {
-      const response = await authenticateUser(credentials);
-      // Handle successful authentication (e.g., store user info, redirect, etc.)
-      console.log(response.message);
-      navigate('/');
-    } catch (error) {
-      console.error('Error authenticating user:', error);
-    }
-  };
-
+function NavBar() {
   return (
-    <div className="AuthWrapper">
-        <div className="userForm">
-        <h1 className="AuthTitle">Sign In</h1>
-        <form onSubmit={handleSubmit}>
-            <div className="AuthFormInput">
-            <img src={EmailIcon} alt="Email Icon" className="AuthIcon" />
-            <input
-                type="email"
-                value={email}
-                onChange={(e) => setEmail(e.target.value)}
-                placeholder="Enter email"
-                required
-            />
-            </div>
-            <div className="AuthFormInput">
-            <img src={PasswordIcon} alt="Password Icon" className="AuthIcon" />
-            <input
-                type="password"
-                value={password}
-                onChange={(e) => setPassword(e.target.value)}
-                placeholder="Enter password"
-                required
-            />
-            </div>
-            <div className="ButtonWrapper">
-                <button type="submit" className="SubmitButton">Sign In</button>
-            </div>
-            <p onClick={toggleForm} className="AuthInfo" >Don't have an account? Sign up</p>
-        </form>
-        </div>
-    </div>
+    <>
+      <Navbar bg="dark" data-bs-theme="dark">
+        <Container>
+          <Navbar.Brand href="/">Novel</Navbar.Brand>
+          <Nav className="me-auto">
+            <Nav.Link href="/">Home</Nav.Link>
+            <Nav.Link href="/">Featured</Nav.Link>
+            <Nav.Link href="/about">About</Nav.Link>
+          </Nav>
+          <Nav>
+            <Nav.Link href="/profile">Profile</Nav.Link>
+            <Nav.Link href="/authentication">Sign In</Nav.Link>
+          </Nav>
+        </Container>
+      </Navbar>
+    </>
   );
-};
+}
 
-export default SignIn;
+export default NavBar;
